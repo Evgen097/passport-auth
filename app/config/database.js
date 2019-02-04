a733e30755b553e69166d26516fed9b79cebb45a
Evgen097 <k0990245853@gmail.com>
2019-02-04 10:52:13 +0200

    first commit

---
+++ app/config/database.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    'url' : 'mongodb://Evgen_0977:@ds121295.mlab.com:21295/auth-local' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
